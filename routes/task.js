95a2c6f3a57cb2731a28233e989155525b268df4
Maria Eduarda <133064900+MariaEduardaFB@users.noreply.github.com>
2023-12-31 17:17:58 -0300

    feat: tasks models

---
+++ routes/task.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const taskScheme = new mongoose.Schema({
+    title:{ type: String, required: true},
+    userId:{ type: mongoose.Schema.Types.ObjectId, ref: 'User', required:true}
+})
+
+const Task = mongoose.model('Task', taskSchema)
+
+module.exports = Task
